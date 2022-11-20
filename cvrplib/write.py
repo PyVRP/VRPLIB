2cbf6b6af84fb54908563c01221ea48a7c953624
l-lan <l.lan@vu.nl>
2022-11-20 09:54:39 +0100

    Working version of write feature

--- cvrplib/write.py
+++ cvrplib/write.py
@@ -1,49 +1,69 @@
+from typing import Dict, Iterable, Union
+
 import numpy as np
 
 
-def write(path, instance, name="problem", euclidean=False, is_vrptw=True):
+def write(
+    path: str,
+    specifications: Dict[str, Union[str, int]],
+    sections: Dict[str, Iterable],
+):
+    """
+    Write a VRP instance to file following the LKH-3 VRPLIB format [1].
+
+    path
+        The path of the file.
+    specfications
+        A dictionary of key value pairs, which will be written to file as
+        KEY : VALUE.
+    sections
+        A dictionary of key value pairs, which will be written as sections.
+
+        <KEY>_SECTION
+        1   a   b   c
+        2   d   e   f
+        ...
+
+    References
+    ----------
+    .. [1] Helsgaun, K. (2017). An Extension of the Lin-Kernighan-Helsgaun TSP
+           Solver for Constrained Traveling Salesman and Vehicle Routing
+           Problems.
+           http://webhotel4.ruc.dk/~keld/research/LKH-3/LKH-3_REPORT.pdf
+
+    """
     with open(path, "w") as f:
-        capacity = instance["capacity"]
-        dimension = len(instance["coords"])
-        comment = ...
-        vrp_type = ...
-        write_preamble(
-            f, name, comment, vrp_type, dimension, euclidean, capacity
-        )
+        rows = [f"{k.upper()} : {v}" for k, v in specifications.items()]
+        f.write("\n".join(rows))
+        f.write("\n")
 
-        if not euclidean:
-            write_edge_weights(f, instance["duration_matrix"])
-
-        # Write data sections
-        write_coords(f, instance["coords"])
-        write_demands(f, instance["demands"])
-        write_is_depot(f, instance["is_depot"])
-
-        if is_vrptw:
-            write_service_times(f, instance["service_times"])
-            write_time_windows(f, instance["time_windows"])
-            write_release_times(f, instance["release_times"])
+        for section_name, data in sections.items():
+            write_section(f, section_name.upper(), data)
 
         f.write("EOF\n")
 
 
-def write_preamble(f, name, comment, vrp_type, dimension, euclidean, capacity):
-    preamble = [
-        ("NAME", name),
-        ("COMMENT", comment),
-        # For HGS we need an extra row... # TODO check this
-        ("TYPE", vrp_type),
-        ("DIMENSION", dimension),
-        ("EDGE_WEIGHT_TYPE", "EUC_2D" if euclidean else "EXPLICIT"),
-        ([] if euclidean else [("EDGE_WEIGHT_FORMAT", "FULL_MATRIX")]),
-        [("CAPACITY", capacity)],
-    ]
+def write_section(f, name: str, data: Iterable):
+    """
+    Write a data section to file. A data section starts with the section name
+    in all uppercase. It is then followed by rows corresponding to the data
+    entries. Each row starts with the index (starting at 1) and the data
+    elements are separated by tabs.
+    """
+    # Sections that have a different format
+    if name == "EDGE_WEIGHT":
+        write_edge_weight_section(f, data)
+    elif name == "DEPOT":
+        write_depot_section(f, data)
+    else:
+        f.write(f"{name}_SECTION\n")
 
-    f.write("\n".join([f"{k} : {v}" for k, v in preamble]))
-    f.write("\n")
+        for idx, elts in enumerate(data, 1):
+            row = f"{idx}\t" + "\t".join(str(elt) for elt in elts)
+            f.write(row + "\n")
 
 
-def write_edge_weights(f, duration_matrix):
+def write_edge_weight_section(f, duration_matrix):
     f.write("EDGE_WEIGHT_SECTION\n")
 
     for row in duration_matrix:
@@ -51,44 +71,9 @@ def write_edge_weights(f, duration_matrix):
         f.write("\n")
 
 
-def write_coords(f, coords):
-    f.write("NODE_COORD_SECTION\n")
-    f.write(
-        "\n".join([f"{idx}\t{x}\t{y}" for idx, (x, y) in enumerate(coords, 1)])
-    )
-    f.write("\n")
-
-
-def write_demands(f, demands):
-    write_section(f, "DEMAND", demands)
-
-
-def write_is_depot(f, is_depot):
+def write_depot_section(f, depots):
     f.write("DEPOT_SECTION\n")
 
-    for i in np.flatnonzero(is_depot):
+    for i in np.flatnonzero(depots):
         f.write(f"{i+1}\n")
         f.write("-1\n")
-
-
-def write_service_times(f, service_times):
-    write_section(f, "SERVICE_TIME", service_times)
-
-
-def write_time_windows(f, tw):
-    f.write("TIME_WINDOW_SECTION\n")
-    f.write(
-        "\n".join([f"{idx}\t{l}\t{u}" for idx, (l, u) in enumerate(tw, 1)])
-    )
-    f.write("\n")
-
-
-def write_release_times(f, release_times):
-    write_section(f, "RELEASE_TIME", release_times)
-
-
-def write_section(f, name, data):
-    # TODO make this work for data with more than 1 dimension
-    f.write(f"{name}_SECTION\n")
-    f.write("\n".join([f"{idx}\t{s}" for idx, s in enumerate(data, 1)]))
-    f.write("\n")
