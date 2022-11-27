465aa66ffb31166d48755c49b02504ce036aa7d7
l-lan <l.lan@vu.nl>
2022-11-27 09:46:57 +0100

    Test error-free reading of LKH solutions

--- tests/read/test_read_solution.py
+++ tests/read/test_read_solution.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+
 import pytest
 
 from cvrplib import read_solution
 
-from .._utils import selected_cases
+from .._utils import LKH_3_DATA_DIR, selected_cases
 
 
 @pytest.mark.parametrize("case", selected_cases())
@@ -12,3 +14,14 @@ def test_read_solution(case):
     """
     solution = read_solution(case.solution_path)
     assert solution["cost"] == pytest.approx(case.cost, 2)
+
+
+@pytest.mark.parametrize(
+    "path", Path(LKH_3_DATA_DIR).glob("*/SOLUTIONS/*.sol")
+)
+def test_lkh_3_vrplib(path):
+    """
+    TODO Maybe add more solutions
+    TODO Test for solution values
+    """
+    read_solution(path)
