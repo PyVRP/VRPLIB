3489e0387ae37133c5ab914f73be6964d553bf16
l-lan <l.lan@vu.nl>
2022-11-28 21:26:30 +0100

    Add dummy test for solution, revert parse_solution

--- tests/read/test_read_solution.py
+++ tests/read/test_read_solution.py
@@ -8,6 +8,37 @@ from cvrplib import read_solution
 from .._utils import LKH_3_DATA_DIR, selected_cases
 
 
+def test_dummy(tmp_path):
+    """
+    Tests if writing a dummy solution yields the correct result.
+    """
+    name = "test.sol"
+
+    with open(tmp_path / name, "w") as fi:
+        solution = "\n".join(
+            [
+                "Route 1 : 1 2",
+                "Route 2 : 3 4",
+                "Route 3 : 5",
+                "Cost : 100",
+                "Time : 123.45",
+                "Name : test.sol",
+                "Comment : Test.",
+            ]
+        )
+        fi.write(solution)
+
+    target = dict(
+        routes=[[1, 2], [3, 4], [5]],
+        cost=100,
+        time=123.45,
+        name=name,
+        comment="Test.",
+    )
+
+    assert_equal(read_solution(tmp_path / name), target)
+
+
 @pytest.mark.parametrize("case", selected_cases())
 def test_read_solution(case):
     """
