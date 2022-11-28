ea87c4f7adae1c6b76c664e3e9e6ea93ae1d4c9a
l-lan <l.lan@vu.nl>
2022-11-28 21:30:30 +0100

    Fix: do not make values lowercase in solution parsing

--- cvrplib/parse/parse_solution.py
+++ cvrplib/parse/parse_solution.py
@@ -24,15 +24,13 @@ def parse_solution(text: str) -> Solution:
     solution: Solution = {"routes": []}
 
     for line in lines:
-        line = line.lower()
-
-        if "route" in line:
+        if "Route" in line:
             route = [int(idx) for idx in line.split(":")[1].split(" ") if idx]
             solution["routes"].append(route)  # type: ignore
         elif ":" in line or " " in line:  # Split at first colon or whitespace
             split_at = ":" if ":" in line else " "
             k, v = [word.strip() for word in line.split(split_at, 1)]
-            solution[k] = infer_type(v)
+            solution[k.lower()] = infer_type(v)
         else:  # Ignore lines without keyword-value pairs
             continue
 
