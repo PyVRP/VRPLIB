885f9cce19bb5440a6c4c1ee899754d3b0c42728
l-lan <l.lan@vu.nl>
2023-02-03 10:25:13 +0100

    Remove write module

--- cvrplib/__init__.py
+++ cvrplib/__init__.py
@@ -1,3 +1,2 @@
 from .download import download_instance, download_solution, list_names
 from .read import read_instance, read_solution
-from .write import write_instance, write_solution
--- cvrplib/write/__init__.py
+++
@@ -1,2 +0,0 @@
-from .write_instance import write_instance
-from .write_solution import write_solution
