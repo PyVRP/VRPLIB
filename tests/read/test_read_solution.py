d110917a5b17e9c677274c0fd0fcb0d8300c9acf
l-lan <l.lan@vu.nl>
2023-02-05 16:21:16 +0100

    Remove old read_solution tests

--- tests/read/test_read_solution.py
+++ tests/read/test_read_solution.py
@@ -1,11 +1,6 @@
-from pathlib import Path
-
-import pytest
 from numpy.testing import assert_equal
 
 from cvrplib import read_solution
-
-from .._utils import LKH_3_DATA_DIR, selected_cases
 
 
 def test_read_dummy_solution(tmp_path):
@@ -37,23 +32,3 @@ def test_read_dummy_solution(tmp_path):
     )
 
     assert_equal(read_solution(tmp_path / name), target)
-
-
-@pytest.mark.parametrize("case", selected_cases())
-def test_read_solution(case):
-    """
-    Read the case solution and verify its cost.
-    """
-    solution = read_solution(case.solution_path)
-    assert_equal(solution["cost"], case.cost)
-
-
-@pytest.mark.parametrize(
-    "path", Path(LKH_3_DATA_DIR).glob("*/SOLUTIONS/*.sol")
-)
-def test_lkh_3_vrplib(path):
-    """
-    TODO Maybe add more solutions
-    TODO Test for solution values
-    """
-    read_solution(path)
