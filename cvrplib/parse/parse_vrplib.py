6f31b6ff72cec23011a2c519f3f49266a698c69d
l-lan <l.lan@vu.nl>
2023-02-05 13:09:46 +0100

    Refactor and test parse_vrplib

--- cvrplib/parse/parse_vrplib.py
+++ cvrplib/parse/parse_vrplib.py
@@ -1,21 +1,25 @@
 import re
-from collections import defaultdict
-from typing import Dict, List, Union
+from typing import Dict, List, Tuple, Union
 
 import numpy as np
 
 from .parse_distances import parse_distances
 from .parse_utils import infer_type, text2lines
 
-Instance = Dict[str, Union[str, int, float, np.ndarray]]
+Instance = Dict[str, Union[str, float, np.ndarray]]
 Lines = List[str]
 
 
 def parse_vrplib(text: str) -> Instance:
     """
-    Parses the instance text. An instance consists of two main parts:
-    - Problem specifications (name, dimension, edge_weight_type, ...)
-    - Data sections (node coords, demands, time windows, ...)
+    Parses a VRPLIB instance. An instance consists of two parts:
+    1) Specifications: single line of the form <KEY>:<VALUE>.
+    2) Data sections: starts with <KEY>_SECTION, followed by rows of values.
+
+    Each specification is parsed as keyword-value pair, where the keyword is
+    given lowercase and the value is unmodified. From each data section the
+    name is parsed in lower case without the "_SECTION". The data values are
+    parsed as numpy arrays, where customer indices are removed (if applicable).
 
     Parameters
     ----------
@@ -24,44 +28,93 @@ def parse_vrplib(text: str) -> Instance:
 
     Returns
     -------
-    The instance data as dictionary.
+    dict
+        The instance data.
     """
     instance: Instance = {}
-    sections = defaultdict(list)  # Store and parse section data later
-    section_name = None  # Used as key to store section data
 
-    for line in text2lines(text):
+    specs, sections = group_specifications_and_sections(text2lines(text))
+
+    for spec in specs:
+        key, value = parse_specification(spec)
+        instance[key] = value
+
+    for section in sections:
+        section, data = parse_section(section, instance)
+        instance[section] = data
+
+    return instance
+
+
+def group_specifications_and_sections(lines: List[str]):
+    """
+    Groups instance lines into specifications and section parts.
+    """
+    specs = []
+    sections = []
+
+    end_section = 0
+    for idx, line in enumerate(lines):
+        if idx < end_section:  # Skip all lines of the current section
+            continue
+
         if "EOF" in line:
             break
 
         if ":" in line:
-            k, v = [x.strip() for x in re.split("\\s*: ", line, maxsplit=1)]
-            instance[k.lower()] = infer_type(v)
-        elif "_SECTION" in line:
-            section_name = line.split("_SECTION")[0].strip()
-        elif section_name is not None:
-            row = [infer_type(num) for num in line.split()]
+            specs.append(line)
 
-            # Most sections start with an index that we do not want to keep
-            if section_name not in ["EDGE_WEIGHT", "DEPOT"]:
-                row = row[1:]
+        if "_SECTION" in line:
+            start = lines.index(line)
+            end_section = start + 1
 
-            sections[section_name.lower()].append(row)
+            for next_line in lines[start + 1 :]:
+                # The current section ends when a next section is found or
+                # when an EOF token is found.
+                if "_SECTION" in next_line or "EOF" in next_line:
+                    break
 
-    for name, data_ in sections.items():
-        data = np.array(data_)
+                end_section += 1
 
-        if data.ndim > 1 and data.shape[-1] == 1:
-            data = data.squeeze(-1)
+            sections.append(lines[start:end_section])
 
-        if name == "depot":
-            # Normalize to start at zero and strip end token
-            data = data[:-1] - 1
+    return specs, sections
 
-        instance[name] = data
 
-    # Post-process edge weights (e.g., compute Euclidean distances from
-    # node coords, or create a full distance matrix from a triangular one).
-    instance["edge_weight"] = parse_distances(instance)
+def parse_specification(line: str) -> Tuple[str, Union[float, str]]:
+    """
+    Parses a specification line as keyword-value pair, split at the first colon
+    occurrence. The keyword is made lowercase and the value is unmodified.
+    """
+    k, v = [x.strip() for x in re.split("\\s*:\\s*", line, maxsplit=1)]
+    return k.lower(), infer_type(v)
 
-    return instance
+
+def parse_section(lines: List, instance: Dict) -> np.ndarray:
+    """
+    Parses the data section into numpy arrays.
+    """
+    section = _remove_suffix(lines[0].strip(), "_SECTION").lower()
+
+    if section == "edge_weight":
+        # Parse separately because it may require additional processing
+        return section, parse_distances(lines[:1], instance)
+
+    data = np.array(lines[1:])
+
+    if section == "depot":
+        # Remove end token and renormalize depots to start at zero
+        data = data[:-1] - 1
+    else:
+        # We remove the customer indices column from non-depot section
+        data = data[:, 1:]
+
+    # Squeeze data sections that contain only one column
+    if data.ndim > 1 and data.shape[-1] == 1:
+        data = data.squeeze(-1)
+
+    return section, data
+
+
+def _remove_suffix(name: str, suffix: str):
+    return name[: -len(suffix)] if name.endswith(suffix) else name
---
+++ tests/parse/test_parse_vrplib.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_equal
+
+from cvrplib.parse.parse_vrplib import (
+    group_specifications_and_sections,
+    parse_section,
+    parse_specification,
+)
+
+
+def test_group_specifications_and_sections():
+    """
+    Check if instance lines are correctly grouped into specifications
+    and sections.
+    """
+    specs = [
+        "NAME : ORTEC-VRPTW-ASYM-00c5356f-d1-n258-k12",
+        "COMMENT : ORTEC",
+    ]
+    sections = [
+        "EDGE_WEIGHT_SECTION",
+        "0	1908",
+        "1994	0",
+        "TIME_WINDOW_SECTION",
+        "1	0	41340",
+        "2	15600	23100",
+    ]
+
+    lines = specs + sections + ["EOF"]
+    actual_specs, actual_sections = group_specifications_and_sections(lines)
+
+    assert_equal(actual_specs, specs)
+    assert_equal(actual_sections, [sections[:3], sections[3:]])
+
+
+@pytest.mark.parametrize(
+    "line, key, value",
+    [
+        ("NAME : Antwerp 1", "name", "Antwerp 1"),  # Whitespace around :
+        ("COMMENT:'test' ", "comment", "'test'"),  # No whitespace around :
+        ("COMMENT: BKS:1", "comment", "BKS:1"),  # Split at first :
+        ("CAPACITY: 30", "capacity", 30),  # int value
+        ("CAPACITY: 30.5", "capacity", 30.5),  # float value
+        ("name: Antwerp 1", "name", "Antwerp 1"),  # OK if key is not uppercase
+    ],
+)
+def test_parse_specification(line, key, value):
+    """
+    Tests if a specification line is correctly parsed.
+    """
+    k, v = parse_specification(line)
+
+    assert_equal(k, key)
+    assert_equal(v, value)
+
+
+@pytest.mark.parametrize(
+    "lines, desired",
+    [
+        (
+            ["SERVICE_TIME_SECTION", [1, 2], [2, 3], [3, 100]],
+            ["service_time", np.array([2, 3, 100])],
+        ),
+        (
+            ["TIME_WINDOW_SECTION", [1, 2, 3], [2, 1, 2]],
+            ["time_window", np.array([[2, 3], [1, 2]])],
+        ),
+        (
+            ["DEPOT_SECTION", [1], [-1]],
+            ["depot", np.array([0])],
+        ),
+    ],
+)
+def test_parse_section(lines, desired):
+    """
+    Tests if data sections (excluding edge weights) are parsed correctly.
+    """
+    actual = parse_section(lines, {})
+
+    assert_equal(actual, desired)
