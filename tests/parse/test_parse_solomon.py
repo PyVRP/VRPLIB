96f639483b6ec06eb3622270d2df89f514cf6a40
l-lan <l.lan@vu.nl>
2023-02-06 16:23:44 +0100

    Test parse solomon

---
+++ tests/parse/test_parse_solomon.py
@@ -0,0 +1,38 @@
+import numpy as np
+from numpy.testing import assert_equal
+
+from vrplib.parse.parse_solomon import parse_solomon
+
+_INSTANCE = [
+    "C101",
+    "VEHICLE",
+    "NUMBER     CAPACITY",
+    "25         200",
+    "CUSTOMER",
+    "CUST NO.  XCOORD.   YCOORD.  DEMAND   READY TIME  DUE DATE  SERVICE TIME",
+    "0      40         50          0          0       1236          0",
+    "1      45         68         10        912        967         90",
+]
+
+
+def test_parse_vrplib():
+    """
+    Checks if the Solomon instance lines are correctly parsed.
+    """
+    text = "\n".join(_INSTANCE)
+
+    actual = parse_solomon(text)
+
+    dist = ((40 - 45) ** 2 + (50 - 68) ** 2) ** 0.5  # from 0 to 1
+    desired = {
+        "name": "C101",
+        "vehicles": 25,
+        "capacity": 200,
+        "node_coord": np.array([[40, 50], [45, 68]]),
+        "demand": np.array([0, 10]),
+        "time_window": np.array([[0, 1236], [912, 967]]),
+        "service_time": np.array([0, 90]),
+        "edge_weight": np.array([[0, dist], [dist, 0]]),
+    }
+
+    assert_equal(actual, desired)
