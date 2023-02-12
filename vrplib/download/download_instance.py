b82ac1b953f62a10d78ae1e4773081ecc7c6fea4
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-02-12 16:26:51 +0100

    Add timeout to download functions (#75)

--- vrplib/download/download_instance.py
+++ vrplib/download/download_instance.py
@@ -17,7 +17,8 @@ def download_instance(name: str, path: str):
         The path where the instance file should be saved.
     """
     ext = "txt" if is_vrptw(name) else "vrp"
-    response = urlopen(CVRPLIB_URL + f"{find_set(name)}/{name}.{ext}")
+    url = CVRPLIB_URL + f"{find_set(name)}/{name}.{ext}"
+    response = urlopen(url, timeout=30)
 
     instance_text = response.read().decode("utf-8")
 
