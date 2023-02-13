6f3c060b8777b832be8ba2d485f9ed1e51e328fa
l-lan <l.lan@vu.nl>
2023-02-13 17:48:42 +0100

    Extend download_set

--- vrplib/download/download_set.py
+++ vrplib/download/download_set.py
@@ -1,10 +1,16 @@
 import urllib
 import zipfile
 
+from .constants import CVRP_SETS, VRPTW_SETS
+
 
 def download_set(set_name, out_dir):
-    url = "http://vrp.atd-lab.inf.puc-rio.br/media/com_vrp/instances/Vrp-Set-Solomon.zip"
+    if set_name not in CVRP_SETS + VRPTW_SETS:
+        raise ValueError(f"Instance set {set_name} is not known.")
 
+    # TODO find set name
+    url = "http://vrp.atd-lab.inf.puc-rio.br/media/com_vrp/instances/Vrp-Set-Solomon.zip"
     zip_path, _ = urllib.request.urlretrieve(url)
+
     with zipfile.ZipFile(zip_path, "r") as f:
         f.extractall(out_dir)
