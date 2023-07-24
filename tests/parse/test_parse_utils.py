6949d21afbaa95b6e039c4f80957bb26e915756b
Niels Wouda <nielswouda@gmail.com>
2023-07-24 10:30:46 +0200

    Allow line comments in files

---
+++ tests/parse/test_parse_utils.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import pytest
+from numpy.testing import assert_equal
+
+from vrplib.parse.parse_utils import text2lines
+
+
+@pytest.mark.parametrize(("text", "expected"), [("", []), ("\n", [])])
+def test_empty_lines(text: str, expected: List[str]):
+    assert_equal(text2lines(text), expected)
+
+
+@pytest.mark.parametrize(
+    ("text", "expected"),
+    [
+        ("# test comment", []),
+        ("# other comment", []),
+        ("123\n#comment", ["123"]),
+        ("#\n#", []),
+        # Lines are stripped before they're inspected for comments, so this
+        # should also be OK:
+        (" # comment after whitespace", []),
+    ],
+)
+def test_comments(text: str, expected: List[str]):
+    assert_equal(text2lines(text), expected)
--- vrplib/parse/parse_utils.py
+++ vrplib/parse/parse_utils.py
@@ -3,9 +3,14 @@ from typing import List, Union
 
 def text2lines(text: str) -> List[str]:
     """
-    Takes a string and returns a list of non-empty, stripped lines.
+    Takes a string and returns a list of non-empty, stripped lines. Also
+    removes any comment lines from the given string.
     """
-    return [line.strip() for line in text.splitlines() if line.strip()]
+    return [
+        stripped
+        for line in text.splitlines()
+        if (stripped := line.strip()) and not stripped.startswith("#")
+    ]
 
 
 def infer_type(s: str) -> Union[int, float, str]:
