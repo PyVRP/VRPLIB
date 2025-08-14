dfc58ea528bb9baf8b1cc21bf06ef1557b8c6881
Leon Lan <l.lan@vu.nl>
2025-08-14 15:37:01 +0200

    Remove download functionality

--- vrplib/download/__init__.py
+++
@@ -1,3 +0,0 @@
-from .download_instance import download_instance as download_instance
-from .download_solution import download_solution as download_solution
-from .list_names import list_names as list_names
