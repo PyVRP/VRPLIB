b2069d13d75579083723729ea8ce3dc018f05f60
Leon Lan <l.lan@vu.nl>
2025-08-14 15:38:23 +0200

    Remove download references

--- vrplib/__init__.py
+++ vrplib/__init__.py
@@ -1,6 +1,3 @@
-from .download import download_instance as download_instance
-from .download import download_solution as download_solution
-from .download import list_names as list_names
 from .read import read_instance as read_instance
 from .read import read_solution as read_solution
 from .write import write_instance as write_instance
